33189c2d87542acbd6fd93b34a446efa8c1dc1d7
Liang Zhang <lzhang@vailsys.com>
2017-09-24 03:04:13 -0500

    add functionality for get number info/get connection info/set up connection

---
+++ controller/get_connection_info.js
@@ -0,0 +1,32 @@
+const getConnectionInfo = function( number ){
+
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.open("GET", "https://api.telnyx.com/security/connections/"+'673546077244228945');
+    xhr.setRequestHeader("x-api-user", "PDcczTxfJyD8HwYdJzAYy4ya");
+    xhr.setRequestHeader("x-api-token", "EzKUrkYMJjM34CeMchrDh71T");
+    xhr.setRequestHeader("cache-control", "no-cache");
+
+    xhr.onload = function () {
+        if (xhr.status !== 200) {
+            console.log(xhr.status + " " + xhr.statusText);
+            return;
+        }
+        try {
+            var response = JSON.parse(xhr.responseText);
+            //var data = parseData(response['result']);
+            console.log(response);
+            //callback(data);
+            //resolve();
+        } catch (e) {
+            // statusChangeCallback({error: e});
+        }
+    };
+    xhr.send(null);
+
+}
+
+module.exports = getConnectionInfo;
+
